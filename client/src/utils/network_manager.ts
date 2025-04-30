25ff4248b40c55817ad78094d44a9e40d905e313
A. Berke Karaman <78508148+berkekaraman99@users.noreply.github.com>
2025-04-30 14:45:22 +0300

    network_manager fix

--- client/src/utils/network_manager.ts
+++ client/src/utils/network_manager.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
+const timeOut = 120000;
+
 export const instance = axios.create({
   baseURL: "http://localhost:3000/api",
-<<<<<<< HEAD
   timeout: import.meta.env.VITE_NETWORK_TIMEOUT ?? timeOut,
-=======
-  timeout: 120000,
->>>>>>> d3afb1a3581f8b2ed04c458c1b3a64fe64cc9e20
 });
